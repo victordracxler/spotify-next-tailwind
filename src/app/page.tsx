057c1967f0856123ecad322f08ca18e1a5ca61d0
Victor Dracxler <victordracxler@poli.ufrj.br>
2023-10-25 17:09:15 -0300

    volume control

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,11 @@ import {
   SkipBack,
   SkipForward,
   Repeat,
+  Mic2,
+  LayoutList,
+  Laptop2,
+  Volume,
+  Maximize2,
 } from "lucide-react";
 import Image from "next/image";
 
@@ -265,7 +270,7 @@ export default function Home() {
           </div>
         </main>
       </div>
-      <footer className='bg-zing-800 border-t border-zinc-700 p-6 flex items-center justify-between'>
+      <footer className='bg-zing-800 border-t border-zinc-700 px-6 py-4 flex items-center justify-between'>
         <div className='flex items-center gap-3'>
           <Image
             src='/album.jpg'
@@ -302,7 +307,18 @@ export default function Home() {
           </div>
         </div>
 
-        <div></div>
+        <div className='flex items-center gap-2'>
+          <Mic2 size={20} />
+          <LayoutList size={20} />
+          <Laptop2 size={20} />
+          <div className='flex items-center gap-2'>
+            <Volume size={20} />
+            <div className='h-1 rounded-full w-24 bg-zinc-600'>
+              <div className='bg-zinc-200 w-10 h-1 rounded-full'></div>
+            </div>
+          </div>
+          <Maximize2 size={20} />
+        </div>
       </footer>
     </div>
   );
